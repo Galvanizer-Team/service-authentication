26639052b0bc3bf4c88d77488240f0a6502f25ad
davidcrammer <david@dbmgrow.com>
2024-03-25 16:29:18 -0700

    Commented out SSL requirement check in requireSSL middleware

--- src/middleware/requireSSL.js
+++ src/middleware/requireSSL.js
@@ -4,7 +4,7 @@ export default function requireSSL(req, res, next) {
   const response = new Response(req, res)
 
   try {
-    if (!req.secure && process.env.NODE_ENV === "production") throw new Error("SSL required")
+    // if (!req.secure && process.env.NODE_ENV === "production") throw new Error("SSL required")
     next()
   } catch (err) {
     response.error(err)
